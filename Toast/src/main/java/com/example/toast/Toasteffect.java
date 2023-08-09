73fa76a139a66d7e50aaa21d033778ceb0e6eb40
Karthik <110925398+Karthik152@users.noreply.github.com>
2023-08-09 21:22:07 +0530

    toaster library added

---
+++ Toast/src/main/java/com/example/toast/Toasteffect.java
@@ -0,0 +1,11 @@
+package com.example.toast;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toasteffect {
+
+    public static void adding(Context context, String mes){
+        Toast.makeText(context, "", Toast.LENGTH_SHORT).show();
+    }
+}
